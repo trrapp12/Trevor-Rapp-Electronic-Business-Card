0683f42e16bef6afc7d91940ce26522c53397eb3
Trevor Rapp <trrapp@gmail.com>
2022-03-09 22:20:47 -0700

    adding different components

---
+++ trevor-rapp-electronic-business-card/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Photo from './Photo'
+import Button from './Button'
+
+
+const App = (props) => {
+  return(
+    <>
+      <Photo />
+      <h1>Welcome</h1>
+
+      <Button 
+        name="Github"
+        action="https://github.com/trrapp12"
+      />
+      <Button 
+        name="LinkedIn"
+        action="https://www.linkedin.com/in/trevor-rapp/"
+      />
+      <Button 
+        name="Email"
+        action="mailto: trrapp@gmail.com"
+      />
+    </>
+
+  )
+}
+
+export default App
