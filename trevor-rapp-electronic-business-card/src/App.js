2a1995e595e4b28f471b7615c9aa22416c9e6455
Trevor Rapp <trrapp@gmail.com>
2022-03-11 01:21:16 -0700

    updates

--- trevor-rapp-electronic-business-card/src/App.js
+++ trevor-rapp-electronic-business-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import LinkedIn from './assets/Vector.svg'
 import Rapp from './Rapp'
 import Photo from './Photo'
 import Button from './Button'
@@ -14,19 +15,21 @@ const App = (props) => {
       <Rapp />
       <Photo />
       <h1>Welcome</h1>
-
-      <Button 
-        name="Github"
-        action="https://github.com/trrapp12"
-      />
-      <Button 
-        name="LinkedIn"
-        action="https://www.linkedin.com/in/trevor-rapp/"
-      />
-      <Button 
-        name="Email"
-        action="mailto: trrapp@gmail.com"
-      />
+      <div className='button--container'>
+        <Button 
+          name="Github"
+          action="https://github.com/trrapp12"
+        />
+        <Button 
+          name="LinkedIn"
+          icon= {LinkedIn}
+          action="https://www.linkedin.com/in/trevor-rapp/"
+        />
+        <Button 
+          name="Email"
+          action="mailto: trrapp@gmail.com"
+        />
+      </div>
       <Content />
       <Footer />
     </div>
--- trevor-rapp-electronic-business-card/src/Button.js
+++ trevor-rapp-electronic-business-card/src/Button.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <form action={props.action} method="get" target="_blank">
-      <button type="submit">{props.name}</button>
-    </form>
+      <form action={props.action} method="get" target="_blank">
+        <button className="button" type="submit">{props.icon}{props.name}</button>
+      </form>
     
   )
 }
