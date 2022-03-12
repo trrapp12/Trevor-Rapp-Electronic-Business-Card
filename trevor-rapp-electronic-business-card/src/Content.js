87e901ed0de8f94134f33507cea080c1d18b2541
Trevor Rapp <trrapp@gmail.com>
2022-03-11 21:17:50 -0700

    fix spelling error

--- trevor-rapp-electronic-business-card/src/Content.js
+++ trevor-rapp-electronic-business-card/src/Content.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Content = (props) => {
   return (
     <div className="content--container">
-      <p>I am a Front-End Developer, Data Analyst, and have an active secret clearance.  I bring a strong portfolio of skills to any web development situation.</p>
+      <p>I am a Front-End Developer, Data Analyst, and I have an active secret clearance.  I bring a strong portfolio of skills to any web development situation.</p>
     </div>
 
   )
