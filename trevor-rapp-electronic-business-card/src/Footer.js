6532803834fe04733b857fbde002fbf532c6da90
Trevor Rapp <trrapp@gmail.com>
2022-03-17 00:27:44 -0600

    fix solution so the  buttons link to href and now have some nice css

--- trevor-rapp-electronic-business-card/src/Footer.js
+++ trevor-rapp-electronic-business-card/src/Footer.js
@@ -4,10 +4,51 @@ import React from 'react'
 
 const Footer = (props) => {
 
-  const iconsArray = ["lni lni-html5","lni lni-css3","lni lni-javascript","lni lni-react","lni lni-nodejs","lni lni-python","lni lni-github-original","lni lni-git","lni lni-angular","lni lni-adobe"];
+  const iconsObjectArray = [
+    {
+      class: "lni lni-html5",
+      url : "https://github.com/trrapp12/Product-Page"
+    },
+    {
+      class: "lni lni-css3",
+      url : "https://github.com/trrapp12/diana-tribute"
+    },
+    {
+      class: "lni lni-javascript",
+      url : "https://github.com/trrapp12/Slap-Happy"
+    },
+    {
+      class: "lni lni-react",
+      url : "https://github.com/trrapp12/React-Jokes"
+    },
+    {
+      class: "lni lni-nodejs",
+      url : "https://trrapp12.github.io/REACT-AirBNB-Clone/"
+    },
+    {
+      class: "lni lni-python",
+      url : "https://github.com/trrapp12/object_oriented_hangman"
+    },
+    {
+      class: "lni lni-github-original",
+      url : "https://github.com/trrapp12"
+    },
+    {
+      class: "lni lni-git",
+      url : "https://github.com/trrapp12"
+    },
+    {
+      class: "lni lni-angular",
+      url : "https://github.com/trrapp12/avengers_agents_of_shield_directory"
+    },
+    {
+      class: "lni lni-adobe",
+      url : "https://github.com/trrapp12/Flex_Box_Photo_Portfolio"
+    }
+  ];
 
-  const iconsArrayOutput = iconsArray.map((input) => {
-    return <i className={input}></i>
+  const iconsArrayOutput = iconsObjectArray.map((input) => {
+    return <div className="button--icon"><a href={input.url} target="_default" ><i className={input.class} ></i></a></div>
   })
 
   return (
