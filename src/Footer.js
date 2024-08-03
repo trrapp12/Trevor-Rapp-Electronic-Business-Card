73b55712d170ef1c878d75001cc8fe43a7a9e926
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2024-08-03 08:01:00 -0600

    update links

--- src/Footer.js
+++ src/Footer.js
@@ -1,65 +1,71 @@
-import React from 'react'
-
-
+import React from 'react';
 
 const Footer = (props) => {
-
   const iconsObjectArray = [
     {
-      class: "lni lni-html5",
-      url : "https://trrapp12.github.io/Product-Page/"
+      class: 'lni lni-html5',
+      url: 'https://trrapp12.github.io/QUADRA-STALKR/',
     },
     {
-      class: "lni lni-css3",
-      url : "https://www.dianarapp.com/"
+      class: 'lni lni-css3',
+      url: 'https://www.dianarapp.com/',
     },
     {
-      class: "lni lni-javascript",
-      url : "https://trrapp12.github.io/Slap-Happy/"
+      class: 'lni lni-javascript',
+      url: 'https://trrapp12.github.io/Slap-Happy/',
     },
     {
-      class: "lni lni-react",
-      url : "https://trrapp12.github.io/React-Jokes/"
+      class: 'lni lni-react',
+      url: 'https://space-rhino.com/',
     },
     {
-      class: "lni lni-nodejs",
-      url : "https://trrapp12.github.io/REACT-AirBNB-Clone/"
+      class: 'lni lni-nodejs',
+      url: 'https://trrapp12.github.io/REACT-AirBNB-Clone/',
     },
     {
-      class: "lni lni-python",
-      url : "https://github.com/trrapp12/object_oriented_hangman"
+      class: 'lni lni-python',
+      url: 'https://github.com/trrapp12/object_oriented_hangman',
     },
     {
-      class: "lni lni-github-original",
-      url : "https://github.com/trrapp12"
+      class: 'lni lni-github-original',
+      url: 'https://github.com/trrapp12',
     },
     {
-      class: "lni lni-git",
-      url : "https://github.com/trrapp12"
+      class: 'lni lni-git',
+      url: 'https://github.com/trrapp12',
     },
     {
-      class: "lni lni-angular",
-      url : "https://trrapp12.github.io/avengers_agents_of_shield_directory/#/list"
+      class: 'lni lni-angular',
+      url: 'https://trrapp12.github.io/avengers_agents_of_shield_directory/#/list',
     },
     {
-      class: "lni lni-adobe",
-      url : "https://github.com/trrapp12/Flex_Box_Photo_Portfolio"
-    }
+      class: 'lni lni-adobe',
+      url: 'https://trrapp.myportfolio.com',
+    },
   ];
-   
+
   const iconsArrayOutput = iconsObjectArray.map((input, index) => {
-    let buttonKey = ("button" + index);
-    let aKey = ("a" + index);
-    let iKey = ("i" + index)
-    return <button key={buttonKey} className="button--icon"><a className="button--a" key={aKey} href={input.url} target="_blank" ><i key={iKey} className={input.class} ></i></a></button>
-  })
+    let buttonKey = 'button' + index;
+    let aKey = 'a' + index;
+    let iKey = 'i' + index;
+    return (
+      <button
+        key={buttonKey}
+        className='button--icon'>
+        <a
+          className='button--a'
+          key={aKey}
+          href={input.url}
+          target='_blank'>
+          <i
+            key={iKey}
+            className={input.class}></i>
+        </a>
+      </button>
+    );
+  });
 
-  return (
-    <div className="footer--container">
-      {iconsArrayOutput}
-    </div>
-  )
-}
+  return <div className='footer--container'>{iconsArrayOutput}</div>;
+};
 
-export default Footer
-
+export default Footer;
